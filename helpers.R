a74be91c43d09e8f324905eb66dd7409b7ee1872
Travis Gerke <tgerke@hsph.harvard.edu>
2019-08-06 13:46:30 -0400

    initialize helpers.R

--- helpers.R
+++ helpers.R
@@ -3,3 +3,19 @@ sample_bin <- function(p) {
    map_int(p, ~ sample(0:1, 1, prob = c(1 - .x, .x)))
 }
 
+model_formula <- function(outcome,predictors) {
+   rlang::new_formula(
+      rlang::parse_expr(outcome),
+      rlang::parse_expr(predictors)
+   )
+}
+
+# simulate binary data
+binary_sim <- function(
+   n = 1000,
+   param = "y ~ 1.67 + 1.50*x1 + 1.2*a1",
+   data = ,
+   family = "binomial",
+) {
+   
+}
